ae86ee4d37fc16e93309f3f184b15c3e4f3175b3
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2023-08-02 10:19:01 +0530

    initial

---
+++ src/main/java/com/cbt/paymentservicecbtaug23one/Paymentservicecbtaug23oneApplication.java
@@ -0,0 +1,13 @@
+package com.cbt.paymentservicecbtaug23one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paymentservicecbtaug23oneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Paymentservicecbtaug23oneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cbt/paymentservicecbtaug23one/Paymentservicecbtaug23oneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbt.paymentservicecbtaug23one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paymentservicecbtaug23oneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
